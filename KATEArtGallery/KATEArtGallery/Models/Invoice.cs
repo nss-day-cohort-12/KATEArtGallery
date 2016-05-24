05d21bfe9df6d835c816138680d9bc568cc66e10
Toni <Toni@TDEW-PC>
2016-05-24 09:47:57 -0700

    added model called Invoice

---
+++ KATEArtGallery/KATEArtGallery/Models/Invoice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KATEArtGallery.Models
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+        public int CustomerId { get; set; }
+        public int AgentId { get; set; }
+        public string PaymentMethod { get; set; }
+        public string ShippingAddress {get; set;}
+        public string PieceSold { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
