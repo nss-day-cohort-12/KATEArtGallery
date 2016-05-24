8bf22000c1e39a55bd9d56de7c7dffe6fef57856
Katlyn Spears <katspears206@gmail.com>
2016-05-24 09:57:43 -0500

    Added the Artwork, IndividualPiece and Customer models

---
+++ KATEArtGallery/KATEArtGallery/Models/ArtWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KATEArtGallery.Models
+{
+    public class ArtWork
+    {
+        public int ArtWorkId { get; set; }
+        public int ArtistId { get; set; }
+        public string Title { get; set; }
+        public string YearOriginallyCreated { get; set; }
+        public string Medium { get; set; }
+        public string Dimensions { get; set; }
+        public int NumberMade { get; set; }
+        public int NumberInInventory { get; set; }
+        public int NumberSold { get; set; }
+    }
+}
