1913c99bd6431d18195ba4e3559154348a9cc4d6
Toni <Toni@TDEW-PC>
2016-05-24 12:08:13 -0700

    added four view models

---
+++ KATEArtGallery/KATEArtGallery/ViewModels/AgentViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KATEArtGallery.ViewModels
+{
+    public class AgentViewModel
+    {
+        public int AgentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int InvoiceId { get; set; }
+        public decimal Price { get; set; }
+        public string PieceSold { get; set; }
+        public int Sold { get; set; }
+        public decimal AgentSales { get; set; }
+        public decimal AgentProfits { get; set; }
+
+    }
+}
