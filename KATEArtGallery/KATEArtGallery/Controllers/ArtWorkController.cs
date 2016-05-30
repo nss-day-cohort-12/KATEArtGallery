e286431529ae3db1b9594e57f4bb8706180df316
eorstrom <eorstrom2010@gmail.com>
2016-05-30 16:48:48 -0500

    artwork data shows up on ViewArtwork page. Also, create new artwork feature works.

--- KATEArtGallery/KATEArtGallery/Controllers/ArtWorkController.cs
+++ KATEArtGallery/KATEArtGallery/Controllers/ArtWorkController.cs
@@ -15,14 +15,60 @@ namespace KATEArtGallery.Controllers
             KATEArtGalleryDBContext _allArtContext = new KATEArtGalleryDBContext();
 
             List<ArtWork> artworkDetails = _allArtContext.Artwork.ToList();
-            //select new ArtWork
-            //{
-            //    Title = art.Title,
-            //    //EmployeeId = emp.EmployeeId,
-            //    //DepartmentName = dept.DepartmentName
-            //}).ToList();
 
             return View(artworkDetails);
         }
+
+        // GET 
+        // [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] // must be here for page to work
+        public ActionResult Create(ArtWork artworkDetails)
+        {
+            using (KATEArtGalleryDBContext _context = new KATEArtGalleryDBContext())
+            {
+                if (ModelState.IsValid)
+                {
+                    ArtWork artwork = new ArtWork
+                    {
+                        Title = artworkDetails.Title,
+                        Category = artworkDetails.Category,
+                        YearOriginalCreated = artworkDetails.YearOriginalCreated,
+                        Medium = artworkDetails.Medium,
+                        Dimensions = artworkDetails.Dimensions,
+                        NumberMade = artworkDetails.NumberMade,
+                        NumberInInventory = artworkDetails.NumberInInventory,
+                        NumberSold = artworkDetails.NumberSold
+                    };
+                    _context.Artwork.Add(artwork);
+                    _context.SaveChanges();
+                    return RedirectToAction("ViewArtwork");
+                }
+                return View(artworkDetails);
+            }
+        }
+        //public ActionResult Delete(int ArtWorkId)
+        //{
+        //    if (ArtWorkId != 0)
+        //    {
+        //        using (KATEArtGalleryDBContext _context = new KATEArtGalleryDBContext())
+        //        {
+        //            ArtWork artwork = _context.Artwork.Find(ArtWorkId);
+
+        //            _context.Artwork.Remove(artwork);
+        //            _context.SaveChanges();
+
+        //        }
+        //    }
+        //    else
+        //    {
+        //        ViewBag.Title = "There was a problem";
+        //    }
+        //    return RedirectToAction("Index");
+        //}
     }
 }
